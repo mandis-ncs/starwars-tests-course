b5621893a458231ab5a3327b2a0f1018d07fbacc
mandis-ncs <amnd.castro@gmail.com>
2024-07-24 11:48:22 -0300

    first commit

---
+++ src/main/java/com/test_api_course/starwars/StarwarsApplication.java
@@ -0,0 +1,13 @@
+package com.test_api_course.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test_api_course/starwars/StarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_api_course.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
